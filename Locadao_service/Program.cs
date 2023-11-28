56ae56d2b8617109bed022906e2c125c6ae7fea2
jcr04 <joaocarlosreboucas15@gmail.com>
2023-11-28 20:19:17 -0300

    add dbcontext

--- Locadao_service/Program.cs
+++ Locadao_service/Program.cs
@@ -1,25 +1,19 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Locadão.Infrastructure.configs;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Adicionar configuração do DbContext
+builder.Services.AddDbContext<LocadaoContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("LocadaoDatabase")));
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+// Outras configurações de serviços, como adicionar controllers, etc.
+// Exemplo: builder.Services.AddControllers();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
+// Configurações do app, como app.UseAuthorization(), app.UseRouting(), etc.
 
 app.Run();
---
+++ Locadão.Infrastructure/configs/LocadaoContext.cs
@@ -0,0 +1,23 @@
+﻿using Locadão.Domain.entitys;
+using Microsoft.EntityFrameworkCore;
+
+namespace Locadão.Infrastructure.configs;
+
+public class LocadaoContext : DbContext
+{
+    public LocadaoContext(DbContextOptions<LocadaoContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Cliente> Clientes { get; set; }
+    public DbSet<Veiculo> Veiculos { get; set; }
+    public DbSet<Locacao> Locacoes { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        // Aqui você pode configurar mapeamentos específicos se necessário
+    }
+}
